d07f06048c86e2b7fd6bf6d4d9cde95d25bdb3bb
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-04-10 10:27:27 -0400

    ship on the left screen and movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -47,10 +47,10 @@ class AlienInvasion:
 
     def _check_keydown_events(self, event):
         """ Respond to key presses """
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
@@ -58,10 +58,10 @@ class AlienInvasion:
 
     def _check_keyup_events(self, event):
         """ Respond to key releases """
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _fire_bullet(self):
         """ Create a new bullet and add it to the bullets group """
