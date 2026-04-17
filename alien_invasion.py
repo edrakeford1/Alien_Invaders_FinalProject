a8d484fab6463bdf2050ed7a615bb1bebc2c4ac3
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-04-17 01:00:36 -0400

    aliens working checkpoint

--- alien_invasion.py
+++ alien_invasion.py
@@ -134,14 +134,14 @@ class AlienInvasion:
         
         current_x, current_y = alien_width, alien_height
 
-        while current_x < (self.settings.screen_height - 2 * alien_height):
-            while current_y < (self.settings.screen_width - 3 * alien_width):
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
                 self._create_alien(current_x, current_y)
-                current_y -= 2 * alien_width
+                current_x += 2 * alien_width
 
             # Finished a row; reset y value and increment x value
-            current_x -= 2 * alien_height
-            current_y = alien_width
+            current_x = alien_width
+            current_y += 2 * alien_width
 
     def _create_alien(self, x_position, y_position):
         """ Create an alien and place it in the fleet """
